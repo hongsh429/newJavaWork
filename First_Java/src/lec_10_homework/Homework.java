7b62e0d926e65111fb7f0b42de223ac1d515524e
hongsh429 <shh0429@kakao.com>
2023-06-12 18:24:54 +0900

    update : update code 1 week homework.java

--- First_Java/src/lec_10_homework/Homework.java
+++ First_Java/src/lec_10_homework/Homework.java
@@ -1,21 +1,21 @@
 package lec_10_homework;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Homework {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         StringBuilder sb = new StringBuilder();
-        String recipe = sc.next();
+        String recipe = sc.nextLine();
         sb.append("[ ").append(recipe).append(" ]").append("\n");
+
         double reviewNum = sc.nextDouble();
+        sc.next();
 
         sb.append("별점 : ").append((int) reviewNum).append(" (").append((int) reviewNum / 5.0 * 100).append(")\n");
         for (int i = 1; i <= 10; i++) {
-            String step = sc.next();
-            sb.append(i).append(". ").append(step).append("\n");
+            sb.append(i).append(". ").append(sc.nextLine()).append("\n");
         }
 
 
